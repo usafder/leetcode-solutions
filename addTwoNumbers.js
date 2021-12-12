b6c68f8fb78cb65a7003366fb7ac9e26d5345dd6
Usman <36886437+usafder@users.noreply.github.com>
2021-12-13 01:14:34 +0500

    Update addTwoNumbers.js

--- addTwoNumbers.js
+++ addTwoNumbers.js
@@ -2,10 +2,10 @@
  * Definition for singly-linked list node.
  */
 class ListNode {
-    constructor(val, next) {
-        this.val = (val === undefined ? 0 : val)
-        this.next = (next === undefined ? null : next)
-    }
+  constructor(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+  }
 }
 
 /**
@@ -14,26 +14,29 @@ class ListNode {
  * @return {ListNode}
  */
 var addTwoNumbers = function (l1, l2) {
-    let l1Node = l1;
-    let l2Node = l2;
-    let l3 = new ListNode(0);
-    let l3Node = l3;
-    let carry = 0;
+  let l1Node = l1;
+  let l2Node = l2;
+  let l3 = new ListNode(0);
+  let l3Node = l3;
+  let carry = 0;
 
-    while (l1Node || l2Node) {
-        const sum = (l1Node?.val ?? 0) + (l2Node?.val ?? 0) + carry
-        const rem = sum % 10;
-        carry = Math.floor(sum / 10);
-        l3Node.val = rem;
-        l3Node.next = new ListNode(0);
-        l3Node = l3Node.next;
-        l1Node = l1Node?.next;
-        l2Node = l2Node?.next;
+  while (l1Node || l2Node) {
+    const sum = (l1Node?.val ?? 0) + (l2Node?.val ?? 0) + carry
+    const rem = sum % 10;
+    carry = Math.floor(sum / 10);
+    l1Node = l1Node?.next;
+    l2Node = l2Node?.next;
+    l3Node.val = rem;
+
+    if (l1Node || l2Node || carry) {
+      l3Node.next = new ListNode(0);
+      l3Node = l3Node.next;
     }
 
     if (carry) {
-        l3Node.val = carry
+      l3Node.val = carry
     }
+  }
 
-    return l3;
-};
+  return l3;
+}
